8e75536471b24234ec7c058b3b82955a36acbf84
Ibe Precious <ibenchedo20@gmail.com>
2022-09-08 16:09:41 -0700

    Size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints exactly "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly
+ * Return: Always ends with 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
