d26247c8d37cef9a606180771dfd0ed86c9803f9
Ibe Precious <ibenchedo20@gmail.com>
2022-09-08 14:37:10 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * The Program prints exactly "Programming is like building a multilingual puzzzle\n"
+ * return 0;
+ */ 
+int main(void)
+{
+	puts("Programming is like bulding a multilingual puzzle\n");
+	return (0);
+}
