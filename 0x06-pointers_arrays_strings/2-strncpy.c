62641740dccf472b51181f52be320aeb37875be6
Ibe Precious <ibenchedo20@gmail.com>
2022-09-21 13:06:55 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - writes a function that copes a string
+ *
+ * @dest: pointer to destination
+ * @src: string to be copied
+ * @n: parameter to use
+ *
+ * Return: always 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (0);
+}
