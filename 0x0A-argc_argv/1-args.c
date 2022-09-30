9e433bfecef43f618f4dd0c9172becf6f10d8d68
Ibe Precious <ibenchedo20@gmail.com>
2022-09-30 14:36:52 -0700

    Silence is argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints a number
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d/n", argc - 1);
+	return (0);
+}
