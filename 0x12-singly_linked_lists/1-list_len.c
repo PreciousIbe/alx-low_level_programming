eb3264a4dc593dbe50c84c0aec7568ade48eeb0d
Ibe Precious <ibenchedo20@gmail.com>
2022-10-21 01:17:48 -0700

    length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
- * list_len - returns the number of number of elements in a list.
+ * list_len - returns the number of elements in a list.
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
-size_T list_len(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t count;
 
 	count = 0;
 	while (h != NULL)
 	{
-		h - h->next;
+		h = h->next;
 		count++;
 	}
 
